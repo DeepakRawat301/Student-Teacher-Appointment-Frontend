2387b3479bc21c1c101ba4dd28b5e5c685cb249e
Deepak Singh Rawat <deepaksinghrawat131@gmail.com>
2025-08-21 19:43:57 +0530

    Update AdminSessionService.js

--- src/services/AdminService/AdminSessionService.js
+++ src/services/AdminService/AdminSessionService.js
@@ -1,43 +1,35 @@
 import axios from 'axios';
 
-const Login_API="http://localhost:8080/login"
+const BASE_URL = "https://student-teacher-appointment-backend.onrender.com";
 
-const USERNAME_API = "http://localhost:8080/api/username";
+const Login_API = `${BASE_URL}/login`;
+const USERNAME_API = `${BASE_URL}/api/username`;
+const LOGOUT_API = `${BASE_URL}/api/logout`;
+const Get_Admin_API = `${BASE_URL}/user/admin/searchByUsername`;
+const Admin_Update_API = `${BASE_URL}/user/admin/update`;
+const Admin_Delete_API = `${BASE_URL}/user/admin/delete`;
+const Admin_Register_API = `${BASE_URL}/public/signup`;
+const Admin_Verification_API = `${BASE_URL}/public/verify`;
 
-const LOGOUT_API = "http://localhost:8080/api/logout";
-
-const Get_Admin_API="http://localhost:8080/user/admin/searchByUsername";
-
-const Admin_Update_API="http://localhost:8080/user/admin/update";
-
-const Admin_Delete_API="http://localhost:8080/user/admin/delete";
-
-const Admin_Register_API="http://localhost:8080/public/signup"
-
-const Admin_Verification_API="http://localhost:8080/public/verify"
-
-
-class AdminSessionService{
-
+class AdminSessionService {
 
     saveAdmin(admin){
-            return axios.post(Admin_Register_API,admin,{
-                withCredentials:true,
-                headers:{
-                    "Content-Type":"application/json"
-                }
-            });
-        }
-    
-        verificationAdmin(admin){
-            return axios.post(Admin_Verification_API,admin,{
-                withCredentials:true,
-                headers:{
-                    "Content-Type":"application/json"
-                }
-            });
-        }
+        return axios.post(Admin_Register_API, admin, {
+            withCredentials:true,
+            headers:{
+                "Content-Type":"application/json"
+            }
+        });
+    }
 
+    verificationAdmin(admin){
+        return axios.post(Admin_Verification_API, admin, {
+            withCredentials:true,
+            headers:{
+                "Content-Type":"application/json"
+            }
+        });
+    }
 
     login(admin) {
         const formData = new URLSearchParams();
@@ -52,33 +44,27 @@ class AdminSessionService{
         });
     }
 
-
     logout() {
         return axios.post(LOGOUT_API, null, { withCredentials: true });
     }
 
     getLoggedInUsername() {
-        return axios.get(USERNAME_API, {
-            withCredentials: true
-        });
+        return axios.get(USERNAME_API, { withCredentials: true });
     }
 
     getAdminByUsername(username) {
-        return axios.get(Get_Admin_API + "?username=" + username,{
-            withCredentials:true
-        });
+        return axios.get(Get_Admin_API + "?username=" + username, { withCredentials:true });
     }
     
     updateAdminByUsername(admin, username) {
         return axios.put(Admin_Update_API, admin, { withCredentials: true });
-
     }
 
     deleteAdminByUsername() {
         return axios.delete(Admin_Delete_API, { withCredentials: true });
     }
     
-    
 }
 
-export default new AdminSessionService();   
+export default new AdminSessionService();
+ 
